72f95d5aa2d71a08b217462bd02b7fd89e489233
Michael-scallon <michael.scallon@ucdconnect.ie>
2022-07-11 21:27:54 +0100

    stops api created

--- Dublinbusapp/urls.py
+++ Dublinbusapp/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from django.urls import path , include
 from . import views
 from django.views.generic import TemplateView
+from rest_framework import routers
+from Dublinbusapp import views
+
+router = routers.DefaultRouter()
+router.register(r'Stops', views.StopsView, 'Stops')
 
 urlpatterns = [
     #path('home/', views.test)
-    path('',TemplateView.as_view(template_name='index.html'))
+    path('',TemplateView.as_view(template_name='index.html')),
+    path('api/', include(router.urls)),
 ]
